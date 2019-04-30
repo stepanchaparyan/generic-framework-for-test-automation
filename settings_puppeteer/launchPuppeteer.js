6b6d035dc2137656813a28540145f65eb68edb57
stepanchaparyan <schaparian@yahoo.com>
2019-04-30 17:47:23 +0400

    travis test update

--- settings_puppeteer/launchPuppeteer.js
+++ settings_puppeteer/launchPuppeteer.js
@@ -2,7 +2,7 @@ import puppeteer from 'puppeteer';
 
 const defaultOptions = {
     headless: true,
-    args: [ '--window-size=1000,1050' ]
+    args: [ '--window-size=1000,1050', '--no-sandbox', '--disable-setuid-sandbox']
     //args: ['--start-maximized'],
     //headless: true, ignoreHTTPSErrors: true
 };
