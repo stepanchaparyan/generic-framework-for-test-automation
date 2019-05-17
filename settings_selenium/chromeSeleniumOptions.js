765e0d1946b422520d004d2dc65f77d3aac83c35
stepanchaparyan <stepanchaparyan@gmail.com>
2019-05-17 18:18:41 +0400

    update package.json, add npm-run-all for fixing Windows OS errors

--- settings_selenium/chromeSeleniumOptions.js
+++ settings_selenium/chromeSeleniumOptions.js
@@ -4,6 +4,9 @@ let chromeOptions = new chrome.Options();
 chromeOptions.addArguments('--headless');
 //options.addArguments('--start-maximized');
 chromeOptions.addArguments('--window-size=1000,1000');
-chromeOptions.addArguments('disable-infobars');
+chromeOptions.addArguments('--disable-infobars');
+//chromeOptions.addArguments('--disable-extensions');
+chromeOptions.addArguments('--log-level=3');
+chromeOptions.addArguments('--desable-gpu');
 
 export default chromeOptions;
