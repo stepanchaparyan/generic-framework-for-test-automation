7d15864b9a22fa0097140415b3def1f3d1e2381b
stepanchaparyan <schaparian@yahoo.com>
2019-06-26 18:53:42 +0400

    change to --headless

--- settings_selenium/chromeSeleniumOptions.js
+++ settings_selenium/chromeSeleniumOptions.js
@@ -1,7 +1,7 @@
 import chrome from 'selenium-webdriver/chrome';
 
 let chromeOptions = new chrome.Options();
-//chromeOptions.addArguments('--headless');
+chromeOptions.addArguments('--headless');
 //options.addArguments('--start-maximized');
 chromeOptions.addArguments('--window-size=1000,1000');
 chromeOptions.addArguments('--disable-infobars');
--- settings_selenium/edgeSeleniumOptions.js
+++ settings_selenium/edgeSeleniumOptions.js
@@ -5,7 +5,7 @@ edgeOptions.set('--headless');
 //edgeOptions.set('--start-maximized');
 edgeOptions.set('--window-size=200,200');
 //edgeOptions.addArguments('disable-infobars');
-edgeOptions.set("--browserSize", "290x500");
+//edgeOptions.set("--browserSize", "290x500");
 
 
 export default edgeOptions;
